804bb01e9644593ea740fb5363346e5e71d06044
AldiAlpaujan <aldi.m.alpaujan.2@gmail.com>
2022-04-20 17:15:36 +0700

    Membuat project open source

---
+++ lib/const.dart
@@ -0,0 +1,3 @@
+//TODO: introduce (work)
+String work =
+    """   Hallo, saya Aldi Alpaujan adalah seorang freelancer programer dalam bidang mobile apps,yang tinggal di daerah bandung lebih tepatnya di daerah kuningan.""";
---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:portofolio/introduction/introduction_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(fontFamily: GoogleFonts.poppins().fontFamily),
+      home: const IntroductionPage(),
+    );
+  }
+}
